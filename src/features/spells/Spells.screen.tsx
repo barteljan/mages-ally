dee9b1f8280f84c291e7040a80ef07b4c9d40abc
Jan Bartel <barteljan@yahoo.de>
2020-02-21 07:47:54 +0100

    added android action button in spells

--- src/features/spells/Spells.screen.tsx
+++ src/features/spells/Spells.screen.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Platform} from 'react-native';
 import {styles} from './Spells.styles';
+import SpellsAddButton from '../spells-add-button/SpellsAddButton.container';
 
 export class SpellsScreen extends React.PureComponent {
   render() {
+    const button = Platform.OS === 'android' ? <SpellsAddButton /> : null;
+
     return (
       <View style={styles.container}>
         <Text>{'Spells'}</Text>
+        {button}
       </View>
     );
   }
